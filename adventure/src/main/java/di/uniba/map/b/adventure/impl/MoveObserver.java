0e07bf978c57c7c4b84f2dc248aeacda40e47710
Francescopio Scarale <80532570+scaraloune@users.noreply.github.com>
2024-06-26 16:44:09 +0200

    aggiornamento porta e uccisione alieno

--- adventure/src/main/java/di/uniba/map/b/adventure/impl/MoveObserver.java
+++ adventure/src/main/java/di/uniba/map/b/adventure/impl/MoveObserver.java
@@ -23,30 +23,33 @@ public class MoveObserver implements GameObserver {
      */
     @Override
     public String update(GameDescription description, ParserOutput parserOutput) {
-        if (parserOutput.getCommand().getType() == CommandType.NORD) {
-            if (description.getCurrentRoom().getNorth() != null) {
-                description.setCurrentRoom(description.getCurrentRoom().getNorth());
-            } else {
-                return "Da quella parte non si può andare c'è un muro!\nNon hai ancora acquisito i poteri per oltrepassare i muri...";
-            }
-        } else if (parserOutput.getCommand().getType() == CommandType.SOUTH) {
-            if (description.getCurrentRoom().getSouth() != null) {
-                description.setCurrentRoom(description.getCurrentRoom().getSouth());
-            } else {
-                return "Da quella parte non si può andare c'è un muro!\nNon hai ancora acquisito i poteri per oltrepassare i muri...";
-            }
-        } else if (parserOutput.getCommand().getType() == CommandType.EAST) {
-            if (description.getCurrentRoom().getEast() != null) {
-                description.setCurrentRoom(description.getCurrentRoom().getEast());
-            } else {
-                return "Da quella parte non si può andare c'è un muro!\nNon hai ancora acquisito i poteri per oltrepassare i muri...";
-            }
-        } else if (parserOutput.getCommand().getType() == CommandType.WEST) {
-            if (description.getCurrentRoom().getWest() != null) {
-                description.setCurrentRoom(description.getCurrentRoom().getWest());
-            } else {
-                return "Da quella parte non si può andare c'è un muro!\nNon hai ancora acquisito i poteri per oltrepassare i muri...";
-            }
+        if (null != parserOutput.getCommand().getType()) switch (parserOutput.getCommand().getType()) {
+            case NORD:
+                if (description.getCurrentRoom().getNorth() != null) {
+                    description.setCurrentRoom(description.getCurrentRoom().getNorth());
+                } else {
+                    return "Da quella parte non si può andare c'è un muro!\nNon hai ancora acquisito i poteri per oltrepassare i muri...";
+                }   break;
+            case SOUTH:
+                if (description.getCurrentRoom().getSouth() != null) {
+                    description.setCurrentRoom(description.getCurrentRoom().getSouth());
+                } else {
+                    return "Da quella parte non si può andare c'è un muro!\nNon hai ancora acquisito i poteri per oltrepassare i muri...";
+                }   break;
+            case EAST:
+                if (description.getCurrentRoom().getEast() != null) {
+                    description.setCurrentRoom(description.getCurrentRoom().getEast());
+                } else {
+                    return "Da quella parte non si può andare c'è un muro!\nNon hai ancora acquisito i poteri per oltrepassare i muri...";
+                }   break;
+            case WEST:
+                if (description.getCurrentRoom().getWest() != null) {
+                    description.setCurrentRoom(description.getCurrentRoom().getWest());
+                } else {
+                    return "Da quella parte non si può andare c'è un muro!\nNon hai ancora acquisito i poteri per oltrepassare i muri...";
+                }   break;
+            default:
+                break;
         }
         return "";
     }
